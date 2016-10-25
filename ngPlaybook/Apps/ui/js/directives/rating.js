b3706d104795a846f051496fbb090a6a752da5a6
Vaibhav Bali <vibsbali@gmail.com>
2016-10-26 09:56:45 +1100

    Created Imperative directive

--- ngPlaybook/Apps/ui/js/directives/rating.js
+++ ngPlaybook/Apps/ui/js/directives/rating.js
@@ -1,11 +1,35 @@
-﻿(function (module) {
+﻿"use strict";
+(function () {
+    angular.module("ui").directive("otfRating", rating);
 
-    var rating = function () {
+    function rating() {
         return {
-            
+            //templateUrl: "/apps/ui/templates/rating.html",
+            scope: {
+                value: "="
+            },
+            link: function (scope, element, attributes) {
+                var min = parseInt(attributes.min || "1");
+                var max = parseInt(attributes.max || "10");
+                scope.$watch("value",
+                    function (newValue) {
+                        element.empty();
+                        for (var i = 0; i < newValue; i++) {
+                            element
+                                .append("<button class='btn btn-default btn-xs'><span class='glyphicon glyphicon-star'></span></button>");
+                        }
+                    });
+                element.on("click",
+                    function () {
+                        scope.$apply(function () {
+                            if (scope.value < max) {
+                                scope.value += 1;
+                            } else {
+                                scope.value = min;
+                            }
+                        });
+                    });
+            }
         };
     };
-
-    module.directive("rating", rating);
-
-}(angular.module("ui")));
+}());
