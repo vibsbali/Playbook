bf379eaafe01aeabfb4c92140f969425998dca66
Vaibhav Bali <vibsbali@gmail.com>
2016-10-26 12:22:11 +1100

    Completed stars directive

--- ngPlaybook/Apps/ui/js/directives/rating.js
+++ ngPlaybook/Apps/ui/js/directives/rating.js
@@ -1,34 +1,70 @@
 ﻿"use strict";
 (function () {
+
+    angular.module("ui")
+        .controller("otfRatingController",
+            function($scope) {
+
+                this.initialize = function(min, max) {
+                    $scope.stars = new Array(max - min + 1);
+                }
+
+                $scope.click = function($index) {
+                    $scope.value = $index + 1;
+                };
+
+                $scope.mouseover = function($index) {
+                    $scope.preview = $index;
+                };
+
+                $scope.mouseout = function() {
+                    $scope.preview = -1;
+                };
+
+                $scope.styles = function($index) {
+                    return {
+                        "glyphicon": true,
+                        "glyphicon-star": $index < $scope.value,
+                        "glyphicon-star-empty": $index >= $scope.value,
+                        "starpreview": $index <= $scope.preview
+                    };
+                };
+            });
+
     angular.module("ui").directive("otfRating", rating);
 
     function rating() {
         return {
-            //templateUrl: "/apps/ui/templates/rating.html",
             scope: {
                 value: "="
             },
-            link: function (scope, element, attributes) {
+            templateUrl: "/apps/ui/templates/rating.html",
+            controller: "otfRatingController",
+            require: "otfRating",
+            link: function (scope, element, attributes, controller) {
                 var min = parseInt(attributes.min || "1");
                 var max = parseInt(attributes.max || "10");
-                scope.$watch("value",
-                    function (newValue) {
-                        element.empty();
-                        for (var i = 0; i < newValue; i++) {
-                            element
-                                .append("<button class='btn btn-default btn-xs'><span class='glyphicon glyphicon-star'></span></button>");
-                        }
-                    });
-                element.on("click",
-                    function () {
-                        scope.$apply(function () {
-                            if (scope.value < max) {
-                                scope.value += 1;
-                            } else {
-                                scope.value = min;
-                            }
-                        });
-                    });
+
+                controller.initialize(min, max);
+
+                //scope.$watch("value",
+                //    function (newValue) {
+                //        element.empty();
+                //        for (var i = 0; i < newValue; i++) {
+                //            element
+                //                .append("<button class='btn btn-default btn-xs'><span class='glyphicon glyphicon-star'></span></button>");
+                //        }
+                //    });
+                //element.on("click",
+                //    function () {
+                //        scope.$apply(function () {
+                //            if (scope.value < max) {
+                //                scope.value += 1;
+                //            } else {
+                //                scope.value = min;
+                //            }
+                //        });
+                //    });
             }
         };
     };
